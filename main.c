09baf7e36f429eaba27361eab8b7b997c8b70834
Thota Sampath <158014181+SampathE7@users.noreply.github.com>
2025-11-30 21:47:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,3 @@
-/* 
-NAME : T.Sampath
-DATE : 28-10-25
-PROJECT : MP3 Tag Reader/Editor
-
-This project is a simple program that lets you read and edit MP3 song details like title, artist, album, year, contents
-and comments. It works from the command line and helps understand how media players store song information such as Title, 
-Artist, Album, Year, Contents and Comment within the MP3 file header. Various Functions such as String functions, 
-File handling functions, Built in functions, Pointers, Loops concepts, 2D arrays 
-and Operators are used to create this project.
-
-Features:
-1. View existing MP3 tag information (-v option)
-2. Edit specific tags such as title, artist, or album (-e option)
-3. File validation and error handling for missing or invalid MP3 files
-4. Uses file I/O and byte-level data manipulation to access tag data
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include "header.h"
